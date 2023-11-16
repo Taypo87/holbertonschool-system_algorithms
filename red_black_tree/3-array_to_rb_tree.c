90c13cc87b73075aa9b79678492548c2237d456e
Taypo87 <taylor.poczynek87@gmail.com>
2023-11-16 11:48:22 -0600

    task 3

---
+++ red_black_tree/3-array_to_rb_tree.c
@@ -0,0 +1,13 @@
+#include "rb_trees.h"
+
+rb_tree_t *array_to_rb_tree(int *array, size_t size)
+{
+    size_t i = 0;
+    rb_tree_t *node;
+
+    while (i < size)
+    {
+        rb_tree_insert(&node, array[i]);
+    }
+    return (node);
+}
