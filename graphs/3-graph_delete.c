39491be9e4fa3c980b3d7fde32d225f8cfe5d2f8
Taypo87 <taylor.poczynek87@gmail.com>
2023-11-18 05:17:11 -0600

    task 3

--- graphs/3-graph_delete.c
+++ graphs/3-graph_delete.c
@@ -7,11 +7,19 @@
 
 void graph_delete(graph_t *graph)
 {
-	vertex_t *temp, *temp2;
-	edge_t *edges, *edge_temp;
+	vertex_t *temp = NULL, *temp2 = NULL;
+	edge_t *edges = NULL, *edge_temp = NULL;
 
+	if (!graph)
+		return;
+	if (!graph->vertices)
+	{
+		free(graph);
+		return;
+	}
 	temp = graph->vertices;
-	edges = temp->edges;
+	if (temp->edges)
+		edges = temp->edges;
 	while (temp)
 	{
 		temp2 = temp;
@@ -20,6 +28,7 @@ void graph_delete(graph_t *graph)
 		{
 			edge_temp = edges;
 			edges = edges->next;
+			free(edge_temp->dest);
 			free(edge_temp);
 		}
 		free(temp2);
