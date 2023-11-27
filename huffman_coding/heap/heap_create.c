6cf82a825da2d2e65f8a5ba4f6a1ce7bee93b765
Taypo87 <taylor.poczynek87@gmail.com>
2023-11-27 04:45:32 -0600

    betty

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,13 +1,17 @@
 #include "heap.h"
 
-
+/**
+ * heap_create - creates a new heap struct
+ * @data_cmp: compares to pointers
+ * Return: pointer to the newly created heap
+*/
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-    heap_t *new = NULL;
+	heap_t *new = NULL;
 
-    new->data_cmp = data_cmp;
-    new->root = NULL;
-    new->size = 0;
+	new->data_cmp = data_cmp;
+	new->root = NULL;
+	new->size = 0;
 
-    return (new);
+	return (new);
 }
