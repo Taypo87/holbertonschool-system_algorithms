fc4a7dfa93c4d0af3fe92a3caeb762613e2a35db
Taypo87 <taylor.poczynek87@gmail.com>
2023-11-30 16:18:45 -0600

    task 5

---
+++ huffman_coding/huffman.h
@@ -0,0 +1,19 @@
+#ifndef _HUFFMAN_
+#define _HUFFMAN_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct symbol_s - Stores a char and its associated frequency
+ *
+ * @data: The character
+ * @freq: The associated frequency
+ */
+typedef struct symbol_s
+{
+    char data;
+    size_t freq;
+} symbol_t;
+
+#endif
---
+++ huffman_coding/symbol.c
@@ -0,0 +1,18 @@
+#include "huffman.h"
+
+/**
+ * symbol_create - initializes a new symbol struct
+ * @data: the data to be stored in the symbol struct
+ * @freq: the frequency at which the data appears
+ * Return: pointer to the newly allocated symbol
+*/
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbol_t *symbol = calloc(1, sizeof(symbol_t));
+	if (!symbol)
+		return (NULL);
+	symbol->data = data;
+	symbol->freq = freq;
+
+	return (symbol);
+}
