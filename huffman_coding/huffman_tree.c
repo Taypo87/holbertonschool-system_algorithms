700494a9ed7e622e46a9f6b59c40486f7026f64b
Taypo87 <taylor.poczynek87@gmail.com>
2023-12-01 06:20:16 -0600

    task 8

---
+++ huffman_coding/huffman_tree.c
@@ -0,0 +1,23 @@
+#include "huffman.h"
+#include "heap.h"
+
+/**
+ * huffman_tree - builds a huffman tree fromn a priority queue
+ * @data: array of characters to be stored in nodes
+ * @freq: array of frequencies to be stored in nodes
+ * Return: pointer to the root of the huffman tree
+*/
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
+{
+	binary_tree_node_t *node;
+	symbol_t *symbol;
+	heap_t *p_queue;
+	size_t i;
+
+	p_queue = huffman_priority_queue(data, freq, size);
+	while (size != 1)
+	{
+		huffman_extract_and_insert(p_queue);
+	}
+	return (p_queue->root);
+}
