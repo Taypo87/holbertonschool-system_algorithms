4f902c9615fe3547bbc820cd344872f4c0043329
Taypo87 <taylor.poczynek87@gmail.com>
2023-12-01 13:47:55 -0600

    updated a few things

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -10,12 +10,16 @@
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
 	heap_t *p_queue;
+	binary_tree_node_t *temp;
 
 	p_queue = huffman_priority_queue(data, freq, size);
-	while (size != 1)
+	if (!p_queue)
+		return (NULL);
+	while (p_queue->size > (size_t)1)
 	{
 		huffman_extract_and_insert(p_queue);
-		size--;
 	}
-	return (p_queue->root);
+	temp = heap_extract(p_queue);
+	heap_delete(p_queue, NULL);
+	return (temp);
 }
