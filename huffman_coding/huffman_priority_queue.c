2c794b5f9b0b5ff9d1a352f748b074767d9ef9e0
Taypo87 <taylor.poczynek87@gmail.com>
2023-12-01 03:34:59 -0600

    saving progress

---
+++ huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,52 @@
+#include "huffman.h"
+#include "heap.h"
+/**
+ * huff_cmp - compares two huffnodes 
+*/
+static int huff_cmp(void *node1, void *node2)
+{
+    binary_tree_node_t *tree1, *tree2;
+    symbol_t *symbol1, *symbol2;
+
+    tree1 = (binary_tree_node_t *)node1;
+    tree2 = (binary_tree_node_t *)node2;
+    symbol1 = tree1->data;
+    symbol2 = tree2->data;
+    return ((int)symbol1->data - (int)symbol2->data);
+
+}
+/**
+ * huff_node_create - creates a node for a huffman tree
+ * @data: character to be stored in a symbol
+ * @size: the frequency of the stored character
+ * Return: pointer to the newly created node
+*/
+static binary_tree_node_t *huff_node_create(char data, size_t freq)
+{
+    symbol_t *symbol;
+
+    symbol = symbol_create(data, freq);
+    return (binary_tree_node(NULL, symbol));
+}
+/**
+ * huffman_priority_queue - creates a priority queue (min heap)
+ * @data: array of characters to be added to the queue
+ * @freq: the associated frequency of data characters
+ * @size: size of the data and freq array
+ * Return: pointer to the priority queue
+*/
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+    heap_t *pqueue;
+    binary_tree_node_t *data;
+    size_t i;
+
+    pqueue = heap_create(huff_cmp);
+
+    for (i = 0; i < size; i++)
+    {
+        data = huff_node_create(data[i], freq[i]);
+        heap_insert(pqueue, data);
+    }
+    return (pqueue);
+}
