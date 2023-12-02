590b0264c80fc5de9deece6b67fcd71c1279b070
Taypo87 <taylor.poczynek87@gmail.com>
2023-12-01 18:16:24 -0600

    trying something out

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -41,11 +41,14 @@ typedef struct queue_s
 	struct queue_s *prev;
 } queue_t;
 
-
-
+binary_tree_node_t *node_swap(binary_tree_node_t *parent, binary_tree_node_t *node);
+binary_tree_node_t *value_check(heap_t *heap, binary_tree_node_t *node);
 heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
 void *heap_extract(heap_t *heap);
 void heap_delete(heap_t *heap, void (*free_data)(void *));
+void heapify(heap_t *heap);
+binary_tree_node_t *get_last_node(const heap_t *heap);
+
 #endif
