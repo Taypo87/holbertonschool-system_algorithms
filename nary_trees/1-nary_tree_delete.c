1c7cf155996b2c384397afbf093e1b11f44c1129
Taypo87 <taylor.poczynek87@gmail.com>
2023-12-18 05:29:42 -0600

    task 1

--- nary_trees/1-nary_tree_delete.c
+++ nary_trees/1-nary_tree_delete.c
@@ -5,16 +5,12 @@
 */
 void nary_tree_delete(nary_tree_t *tree)
 {
-	nary_tree_t *temp;
-	
 	if (!tree)
 		return;
 	if (tree->children)
 		nary_tree_delete(tree->children);
 	if (tree->next)
-	{
 		nary_tree_delete(tree->next);
-	}
 	free(tree->content);
 	free(tree);
 	return;
