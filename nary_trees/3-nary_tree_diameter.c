372f06f8f3459f7b4249b0b98492e01cfb2583f4
Taypo87 <taylor.poczynek87@gmail.com>
2023-12-22 04:20:47 -0600

    rewrite of task 3

--- nary_trees/3-nary_tree_diameter.c
+++ nary_trees/3-nary_tree_diameter.c
@@ -1,6 +1,26 @@
 #include "nary_trees.h"
 
 /**
+ * update_top_two - updates top_two array with the greatest heights
+ * @height: the height to check against the current top two heights
+ * Return: the top two heights
+*/
+static size_t *update_top_two(size_t height)
+{
+	static size_t top_two[2] = {0}, temp = 0;
+
+
+	if (temp > top_two[1])
+		top_two[1] = temp;
+	if (top_two[1] > top_two[0])
+	{
+		temp = top_two[0];
+		top_two[0] = top_two[1];
+		top_two[1] = temp;
+	}
+	return (&top_two);
+}
+/**
  * max - returns the greater value of x and y
  * x: first value
  * y: second value
@@ -40,7 +60,7 @@ static size_t get_height(nary_tree_t *root)
 */
 size_t nary_tree_diameter(nary_tree_t const *root)
 {
-	size_t diameter = 1, *top_two, temp = 0;
+	size_t *top_two, temp = 0;
 	size_t root_diameter = 0, sub_diameter = 0;
 	nary_tree_t *childs;
 
@@ -60,25 +80,5 @@ size_t nary_tree_diameter(nary_tree_t const *root)
 		if (sub_diameter < temp)
 			sub_diameter = temp;
 	}
-	return (diameter > sub_diameter ? diameter : sub_diameter);
-}
-/**
- * update_top_two - updates top_two array with the greatest heights
- * @height: the height to check against the current top two heights
- * Return: the top two heights
-*/
-static size_t *update_top_two(size_t height)
-{
-	static size_t top_two[2] = {0}, temp = 0;
-
-
-	if (temp > top_two[1])
-		top_two[1] = temp;
-	if (top_two[1] > top_two[0])
-	{
-		temp = top_two[0];
-		top_two[0] = top_two[1];
-		top_two[1] = temp;
-	}
-	return (&top_two);
+	return (root_diameter > sub_diameter ? root_diameter : sub_diameter);
 }
